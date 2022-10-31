ebe0ed546f4ffd45f6dddcb6adb22115c7bb84c2
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2022-10-31 17:40:28 +0100

    bootstrapped to mudblazor application

--- Nate.DockerComposeUI.UI/Program.cs
+++ Nate.DockerComposeUI.UI/Program.cs
@@ -1,21 +1,41 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using System.Globalization;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Nate.DockerComposeUI.UI.Data;
+using Serilog;
+using MudBlazor.Services;
+
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+// configure host
+builder.Host.UseSerilog();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(b => { });
+
+// configure webhost
+builder.WebHost.UseSentry();
+builder.WebHost.UseStaticWebAssets();
+
+// configure services
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// configure http pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/error");
     app.UseHsts();
 }
 
